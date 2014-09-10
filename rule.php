d4257334d10a7dd9fb6dcd3a0ec44bd0db7c074b
九条カレン <shiraihii@yahoo.co.jp>
2014-09-10 22:13:15 +0800

    Update rule.php

--- rule.php
+++ rule.php
@@ -8,7 +8,7 @@
 					<div class="col-sm-4">
 						<img src="img/zjy.jpg" alt="联系方式" class="img-responsive">
 						<h3>联系方式</h3>
-						<p><a href="mailto:shiraihii@yahoo.co.jp" target="_blank"></a></p>
+						<p><a href="mailto:shiraihii@yahoo.co.jp" target="_blank">电子邮件</a></p>
 					</div>
 					<div class="col-sm-4">
 						<img src="img/zjy.jpg" alt="注意事项" class="img-responsive">

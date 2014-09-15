61c38450b5452f342fb037afb72921ef514d23cb
九条カレン <shiraihii@yahoo.co.jp>
2014-09-15 21:45:50 +0800

    Update map.php

--- map.php
+++ map.php
@@ -2,6 +2,6 @@
 <?php include('headers.inc.html.php'); ?>
 		<frameset rows="50,*" border=0 frameborder=0 framespacing=0px scrolling="no" noresize> 
 			<frame src="mapfm.php" border=0 frameborder=0 framespacing=0px scrolling="no" noresize>
-			<frame src="http://shiraihii.xicp.net:8122" border=0 frameborder=0 framespacing=0px scrolling="no" noresize>
+			<frame src="http://fzmcsjtu.tk:8122" border=0 frameborder=0 framespacing=0px scrolling="no" noresize>
 		</frameset> 
 </html>
